364276583199c68dceb2f924fad13bdc526276ff
chalscc <carlescatalancalabuig@gmail.com>
2023-11-21 19:44:13 +0100

    Creación de gifs.service

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'gifs-search-box',
@@ -19,11 +20,14 @@ export class SearchBoxComponent {
   public tagInput!: ElementRef<HTMLInputElement>;
 
 
-  constructor() { }
+  constructor( private gifsService: GifsService) { }
 
   searchTag() {
     const newTag = this.tagInput.nativeElement.value;
-    console.log(newTag);
+
+    this.gifsService.searchTag(newTag);
+
+    this.tagInput.nativeElement.value = '';
   }
 
 }
