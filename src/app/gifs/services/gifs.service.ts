de8ad2e3a0c56b34a502066730db9183971cce0b
chalscc <carlescatalancalabuig@gmail.com>
2023-11-22 12:29:10 +0100

    Creación de nueva interfaz de datos Gif

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,8 +1,11 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
 @Injectable({ providedIn: 'root' })
 export class GifsService {
+
+  public gifList: Gif[] = [];
 
   private _tagsHistory: string[] = [];
   private apiKey: string = 'ascTyQ87C60ZxNX9ZQhkTFEwpnRpJJev';
@@ -39,9 +42,9 @@ export class GifsService {
       .set('limit', '10')
       .set('q', tag);
 
-    this.http.get(`${this.serviceUrl}/search?`, { params })
-      .subscribe(resp => {
-        console.log(resp);
+    this.http.get<SearchResponse>(`${this.serviceUrl}/search?`, { params })
+      .subscribe((resp) => {
+        this.gifList = resp.data;
       })
 
   }
