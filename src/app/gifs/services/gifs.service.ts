5383f4e662f182ccad656e07c7cc267d45c82a7e
chalscc <carlescatalancalabuig@gmail.com>
2023-11-22 12:05:14 +0100

    Get Http Request

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,11 +1,15 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class GifsService {
 
   private _tagsHistory: string[] = [];
+  private apiKey: string = 'ascTyQ87C60ZxNX9ZQhkTFEwpnRpJJev';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor() { }
+
+  constructor( private http: HttpClient) { }
 
   get tagsHistory(): string[] {
     return [...this._tagsHistory];
@@ -25,11 +29,20 @@ export class GifsService {
   }
 
 
-  searchTag(tag: string): void {
+  searchTag(tag: string):void {
 
     if (tag.length === 0) return;
-
     this.organizeHistory(tag);
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', tag);
+
+    this.http.get(`${this.serviceUrl}/search?`, { params })
+      .subscribe(resp => {
+        console.log(resp);
+      })
 
   }
 }
