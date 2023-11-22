79d36f5383fc70467b7511bff8a34b0a02114ca9
chalscc <carlescatalancalabuig@gmail.com>
2023-11-22 16:02:19 +0100

    Lazy loading para mostrar la imagen solo cuando ha cargado

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -4,6 +4,7 @@ import { HomePageComponent } from './pages/home/home-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
 import { CardListComponent } from './components/card-list/card-list.component';
 import { GifsCardComponent } from './components/gifs-card/gifs-card.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -14,7 +15,8 @@ import { GifsCardComponent } from './components/gifs-card/gifs-card.component';
     GifsCardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports: [
     HomePageComponent
