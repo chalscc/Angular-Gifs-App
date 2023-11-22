0aad7e56fb862710bada644ba71f250dc184c157
chalscc <carlescatalancalabuig@gmail.com>
2023-11-22 13:12:20 +0100

    Cargamos el LocalStorage en reload

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -12,7 +12,10 @@ export class GifsService {
   private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
 
-  constructor( private http: HttpClient) { }
+  constructor( private http: HttpClient) {
+    this.loadLocalStorage();
+
+  }
 
   get tagsHistory(): string[] {
     return [...this._tagsHistory];
@@ -34,6 +37,16 @@ export class GifsService {
 
   private saveLocalStorage(): void {
     localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+  }
+
+  private loadLocalStorage(): void {
+    if(!localStorage.getItem('history')) return;
+
+    this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
+
+    if(this._tagsHistory.length === 0) return;
+    this.searchTag(this._tagsHistory[0]);
+
   }
 
   searchTag(tag: string):void {
